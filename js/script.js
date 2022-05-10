e7f1c9fe9e52c27eea129a83be9edcf44aeafde7
ban2orsolic <ban.orsolic@outlook.com>
2022-05-11 00:14:47 +0200

    added addon section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+import { version } from '../package.json'; // čitanje broja verzije iz package.json
+console.log('hello!', version);
+
+// set version number on page
+const verEl = document.querySelector('.ver');
+verEl.textContent = version;
+
 // potrebno za development kada se koristi Parcel da bi se stranica ispravno refreshala
 
 if (module.hot) {
