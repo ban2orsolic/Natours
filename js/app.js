236f03f8a4535cb8aadea44690c7d3aa24fd2720
ban2orsolic <ban.orsolic@outlook.com>
2022-05-14 01:28:52 +0200

    added Tours section with rotating cards

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// update release-date in package.json
+// Node.Js skripta koja se izvršava sa npm run update
+// update release-date in package.json, potrebno je instalirati kvjson package
+import { exec } from 'child_process';
 
 const date = `${new Date().getDate()}.${
   new Date().getMonth() + 1
 }.${new Date().getFullYear()}`;
 
 const command = `"./node_modules/.bin/kvjson" package.json release ${date}`;
-
-import { exec } from 'child_process';
 
 exec(command, (error, stdout, stderr) => {
   if (error) {
